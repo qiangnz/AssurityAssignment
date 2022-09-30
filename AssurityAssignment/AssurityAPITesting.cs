629e73f98b3337d740a0d4d997a32a78c8e370b4
QiangNZ <qiang.z@outlook.com>
2022-10-01 11:51:07 +1300

    Access API and Get API response

--- AssurityAssignment/AssurityAPITesting.cs
+++ AssurityAssignment/AssurityAPITesting.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
 using System;
 
 namespace AssurityAssignment
@@ -6,6 +7,19 @@ namespace AssurityAssignment
     [TestClass]
     public class AssurityAPITesting
     {
+        //Access API and get API response details
+        public string GetAPIDetails()
+        {
+            var client = new RestClient("https://api.tmsandbox.co.nz/");
+            var request = new RestRequest("v1/Categories/6328/Details.json?catalogue=false");
+            
+            RestResponse response = client.Execute(request);
+            var getAPIDetails = response.Content;
+
+            return getAPIDetails;
+        }
+
+
         [TestMethod]
         public void TestMethod1()
         {
