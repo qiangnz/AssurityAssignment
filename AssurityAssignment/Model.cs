f1bb6ab9d25ece5978fb0371ce1dc44ab60ae541
QiangNZ <qiang.z@outlook.com>
2022-10-01 11:37:19 +1300

    Add Model

---
+++ AssurityAssignment/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssurityAssignment
+{
+    public class CriteriaContents
+    {
+        public string Name { get; set; }
+
+        public bool CanRelist { get; set; }
+
+        public _Promotions[] Promotions { get; set; }
+
+    }
+
+    public class _Promotions
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # AssurityAssignment
 Assurity Consulting 2023 Graduate Program
 
+##Unit Test Project(.NET Framework)
 .NET Framework 4.7.2
 
-Dependenices:
+##Dependenices
 Newtonsoft.Json 13.0.1
 RestSharp 108.0.2
+
